da4f5456633bf0c96177b0534b4a8ddc441a1195
joe <joedabongo01@gmail.com>
2022-09-08 17:48:24 -0700

    Hello printf

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -14,9 +14,9 @@
 
 int main(void)
 
-{
+{	
 	/*prints a string*/
-	printf("with proper grammar, but the outcome is a piece of art,\n");       
+	printf("with proper grammar, but the outcome is a piece of art,\n");
 	return (0);
 
 }
