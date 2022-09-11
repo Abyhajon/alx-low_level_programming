c7d2821c7a8371c9a8d40383ecf74183237289a8
joe <joedabongo01@gmail.com>
2022-09-11 05:39:46 -0700

    betty fix

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -10,17 +10,16 @@
  *
  *    * Return: Always 0 (success)
  *
- *     */
-
+ */
 int main(void)
-
 {
 
 	char c;
+
 	for (c = 'a'; c <= 'z'; ++c)
 	{
-		if(c != 'e' && c != 'q' )
-			putchar(c);
+		if (c != 'e' && c != 'q')
+			putchar (c);
 	}		
 	
 	putchar('\n');
