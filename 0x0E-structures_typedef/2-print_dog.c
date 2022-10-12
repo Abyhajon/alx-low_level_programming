06feec831eb51186035485a76cf4652e143cf6ec
Abyhajon <joedabongo01@gmail.com>
2022-10-11 23:18:20 -0700

    update

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -12,17 +12,17 @@ if (d == NULL)
 return;
 
 if ((*d).name == NULL)
-printf("Name:(nil)\n");
+printf("Name: (nil)\n");
 else
 printf("Name: %s\n", d->name);
 
 if ((*d).age < 0)
-printf("Age:(nil)\n");
+printf("Age: (nil)\n");
 else
 printf("Age: %f\n", d->age);
 
 if ((*d).owner == NULL)
-printf("Owner:(nill)\n");
+printf("Owner: (nill)\n");
 else
 printf("Owner: %s\n", d->owner);
 
